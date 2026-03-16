f850ded3ab15a14ae5375e2f02cab4153181eca0
w444555888 <acer2041710@gmail.com>
2026-03-16 11:48:04 +0800

    新增部屬，新增跨域網址

--- api/app/main.py
+++ api/app/main.py
@@ -41,7 +41,13 @@ async def on_shutdown():
 # 設定 CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
+        "https://pycrawler-admin.onrender.com",
+        "https://pycrawler-client.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
